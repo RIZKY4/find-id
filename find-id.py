47710d3f760196c95bc3030ca113cc35648b727b
RIZKY4 <66553368+RIZKY4@users.noreply.github.com>
2021-01-29 02:53:42 +0000

    Update find-id.py

--- find-id.py
+++ find-id.py
@@ -1,5 +1,6 @@
-# Coding by fb.com/Rizky.Rasata
-# Python2
+#coding=utf-8
+#coding by fb.com/Rizky.Rasata
+#python2
 import requests,os,re
 
 baner = """ _____ _           _   ___ ____    _____ ____
@@ -9,19 +10,56 @@ baner = """ _____ _           _   ___ ____    _____ ____
 |  _| | | | | | (_| |  | || |_| | |  _| | |_) |
 |_|   |_|_| |_|\__,_| |___|____/  |_|   |____/\n"""
 
-try:
- os.system('clear')
- print baner
- u = raw_input('Masukkan username > ')
- url = 'https://www.facebook.com/'+u
- r = requests.get(url).text
- name = re.search('Title">(.*?)</', r).group(1).strip('| Facebook')
- id = re.search('profile/(.*?)" ', r).group(1)
+def id():
+	try:
+		u = raw_input('\nMasukkan username > ')
+		url = 'https://www.facebook.com/'+u
+		r = requests.get(url).text
+		name = re.search('Title">(.*?)</', r).group(1).strip('| Facebook')
+		id = re.search('profile/(.*?)" ', r).group(1)
+		
+		print '\nNama > '+name
+		print 'Id   > '+id+'\n'
+		exit()
+		 
+	except requests.exceptions.ConnectionError:
+		print '× Koneksi bermasalah'
+		exit()
+	except AttributeError:
+		print '× Username tidak di temukan'
+		exit()
+		
+def user():
+	try:
+		u = raw_input('\nMasukkan id > ')
+		url = 'https://www.facebook.com/'+u
+		r = requests.get(url).text
+		name = re.search('Title">(.*?)</', r).group(1).strip('| Facebook')
+		user = re.search('<span class="word_break"></span>(.*?)</span>', r).group(1)
+		
+		print '\nNama     > '+name
+		print 'Username > '+user+'\n'
+		exit()
+		 
+	except requests.exceptions.ConnectionError:
+		print '× Koneksi bermasalah'
+		exit()
+	except AttributeError:
+		print '× Id tidak di temukan'
+		exit()
+		
+os.system('clear')
+print baner
+print '1. Cari id menggunakan username'
+print '2. Cari username menggunakan id\n'
 
- print '\nNAMA > '+name
- print 'ID   > '+id+'\n'
- 
-except requests.exceptions.ConnectionError:
- 	print 'Koneksi tidak ada'
-except AttributeError:
-	print 'Username tidak di temukan'
+while True:
+	p = raw_input('>> ')
+	if p=="":
+		print '× Masukkan yg benar'
+	elif p=="1":
+		id()
+	elif p=="2":
+		user()
+	else:
+		print '× Masukkan yg benar'
